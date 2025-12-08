e3a4493c62afd57301cf533cda4f66417c1b0441
litengut <litengut@icloud.com>
2025-12-08 13:31:58 +0100

    format and day 6

--- src/1/index.ts
+++ src/1/index.ts
@@ -1,47 +1,51 @@
-
 const StartPossition = 50;
-
 
 const file = Bun.file("./src/1/input.txt");
 const content = await file.text();
 const lines = content.split("\n");
 
 let position = StartPossition;
-let zeros = 0
+let zeros = 0;
 
 for (const line of lines) {
-    const minus = line.at(0) === "L" ? -1 : 1;
-    const number = Number.parseInt(line.substring(1)) * minus
-    const previouspostion = position
-    position += number % 100
+  const minus = line.at(0) === "L" ? -1 : 1;
+  const number = Number.parseInt(line.substring(1)) * minus;
+  const previouspostion = position;
+  position += number % 100;
 
-    let x = Math.floor(Math.abs(number) / 100)
-    let extra = 0
+  let x = Math.floor(Math.abs(number) / 100);
+  let extra = 0;
 
-    
-    if(position < 0){
-        if (previouspostion !== 0) extra++;
-        position = 100 - Math.abs(position % 100)    
+  if (position < 0) {
+    if (previouspostion !== 0) extra++;
+    position = 100 - Math.abs(position % 100);
+  }
+  if (position >= 100) {
+    if (previouspostion !== 0) extra++;
+    position = position % 100;
+  }
 
-        
-    }
-    if(position >= 100){
-        if (previouspostion !== 0) extra++;
-        position = position % 100
-       
-    }
+  // console.log("N",zeros)
+  if (position === 0 && extra === 0) {
+    x++;
+  }
+  x += extra;
 
-
-    // console.log("N",zeros)
-   if(position === 0 && extra === 0){
-    x++
-   }
-   x += extra;
-
-    // console.log("",previouspostion)
-    console.log(previouspostion,"+",number,"=",position, " | ",x,",", extra,"|", zeros )
-    zeros += x
-    
+  // console.log("",previouspostion)
+  console.log(
+    previouspostion,
+    "+",
+    number,
+    "=",
+    position,
+    " | ",
+    x,
+    ",",
+    extra,
+    "|",
+    zeros,
+  );
+  zeros += x;
 }
-console.log("postion",position)
-console.log("zerors",zeros)
+console.log("postion", position);
+console.log("zerors", zeros);
--- src/6/index.ts
+++ src/6/index.ts
@@ -1,21 +1,41 @@
+const file = Bun.file("./src/6/input.txt");
+const conent = await file.text();
+const lines = conent.split("\n");
+const numbers = lines
+  .slice(0, -1)
+  .map((r) => r.split("").map((number) => Number.parseInt(number)));
+const opperators = lines.at(-1)?.split("")!;
+console.log(numbers, opperators);
 
-
-const file = Bun.file("./src/6/input.txt")
-const conent = await file.text()
-const lines = conent.split("\n")
-const numbersarray = lines.slice(0,-1).map((r) => r.split(" ").filter(word => word.length > 0))
-const opperators = lines.at(-1)?.split(" ").filter(word => word.length > 0)!
-console.log(numbersarray,opperators) 
-
-const out = numbersarray[0]?.map((_,i) => {
-    const numbers = numbersarray.map((numbers) => Number.parseInt(numbers[i] || ""))
-    const opperator = opperators[i]!
-    if(opperator === "+"){
-        return numbers.reduce((p,c) => p + c)
-    }else if (opperator === "*"){
-        return numbers.reduce((p,c) => p * c)
+let currentOperator = "";
+let number = 0;
+let sum = 0;
+for (const [i, opperator] of opperators.entries()) {
+  if (opperator !== " ") {
+    currentOperator = opperator;
+    console.log(number);
+    sum += number;
+    number = 0;
+  }
+  const xy = Number.parseInt(
+    numbers
+      .map((row) => {
+        if (Number.isNaN(row[i])) return;
+        return row[i]!;
+      })
+      .filter((x) => x !== undefined)
+      .join(""),
+  );
+  if (Number.isNaN(xy)) continue;
+  // console.log(xy);
+  if (xy !== 0) {
+    if (currentOperator === "+") {
+      number += xy;
+    } else if (currentOperator === "*") {
+      if (number === 0) number = 1;
+      number *= xy;
     }
-    throw new Error("wtf")
-}).reduce((p,c) => p + c)
-
-console.log(out)
+  }
+}
+sum += number;
+console.log("sum", sum);
